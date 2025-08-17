e2cc97da163d44538c927c4e7f53ed8625f76e8a
GabrielAballay25 <80085@sistemas.frc.utn.edu.ar>
2025-08-17 18:04:02 -0300

    Header (Navbar) y Video de Bienvenida

---
+++ rotary-club/src/components/Carousel.jsx
@@ -0,0 +1,26 @@
+const Carousel = () => (
+  <div className="carousel slide" id="carouselExample">
+    <div className="carousel-inner">
+      <div className="carousel-item active">
+        <video
+          src="https://www.rotary.org/sites/default/files/2018_156_PowerInOurConnections_Omnibus30_NOVO_1080-v2.mp4"
+          autoPlay
+          muted
+          loop
+          playsInline
+          className="video-hero"
+        />
+        <div className="carousel-caption d-none d-md-block">
+          <h1 className="fw-bold">Somos gente de acción</h1>
+          <p>
+            Nuestra red mundial de más de 1,2 millones de vecinos, amigos y líderes voluntarios
+            ofrecen sus conocimientos y recursos para resolver problemas y abordar las necesidades de las comunidades.
+          </p>
+          <button className="btn btn-outline-light">Toma acción junto a nosotros</button>
+        </div>
+      </div>
+    </div>
+  </div>
+);
+
+export default Carousel;
---
+++ rotary-club/src/components/Header.jsx
@@ -0,0 +1,67 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+const Header = () => (
+    <nav className="navbar navbar-expand-lg">
+        <div className="container-fluid">
+            <img 
+                src="/rotarylogo1.png"   
+                    alt="Rotary Logo" 
+                    width="225" 
+                    height="70" 
+                className="d-inline-block align-text-top"
+                />
+            
+            <button
+                className="navbar-toggler"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
+            >
+                <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li className="nav-item">
+                        <button type="button" class="btn btn-outline-warning">Inicio</button>
+                    </li>
+                    <li className="nav-item">
+                        <button type="button" class="btn btn-outline-warning">Quienes somos</button>
+                    </li>
+                    <li className="nav-item">
+                        <button type="button" class="btn btn-outline-warning">Calendario de Actividades</button>
+                    </li>
+                    <li className="nav-item">
+                        <button type="button" class="btn btn-outline-warning">Banco Ortopedico</button>
+                    </li>
+                    <li className="nav-item">
+                        <button type="button" class="btn btn-outline-warning">Concurso Literario</button>
+                    </li>
+                    <li className="nav-item dropdown">
+                        <a
+                            className="nav-link dropdown-toggle"
+                            href="#"
+                            role="button"
+                            data-bs-toggle="dropdown"
+                            aria-expanded="false"
+                        >
+                            Intercambio de Jovenes
+                        </a>
+                        <ul className="dropdown-menu">
+                            <li><a className="dropdown-item" href="#">Largo plazo</a></li>
+                            <li><hr className="dropdown-divider" /></li>
+                            <li><a className="dropdown-item" href="#">Corto plazo</a></li>
+                            <li><hr className="dropdown-divider" /></li>
+                            <li><a className="dropdown-item" href="#">Nuevas generaciones</a></li>
+                        </ul>
+                    </li>
+                </ul>
+                <button className="btn btn-outline-success" type="submit">Contactanos</button>
+            </div>
+        </div>
+    </nav>
+);
+
+export default Header;
